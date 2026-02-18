d04c52d83e112c0d3847ec381af5a44301167eba
moshthesubnet <65147322+moshthesubnet@users.noreply.github.com>
2026-02-18 11:23:46 -0700

    Update extra.js

--- docs/javascripts/extra.js
+++ docs/javascripts/extra.js
@@ -1,11 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Auto-apply AOS to common content elements
-  document.querySelectorAll(
-    ".md-content h1, .md-content h2, .md-content h3, .md-content p, .md-content table, .md-content pre, .md-content ul, .md-content ol, .md-content div[align='center']"
-  ).forEach(function (el, index) {
+document.querySelectorAll(
+  ".md-content h1, .md-content h2, .md-content h3, .md-content p, .md-content pre, .md-content ul, .md-content ol, .md-content div[align='center'], .md-content table"
+).forEach(function (el, index) {
+  if (!el.hasAttribute("data-aos")) {
     el.setAttribute("data-aos", "fade-up");
     el.setAttribute("data-aos-delay", (index % 5) * 100);
-  });
+  }
+});
 
   AOS.init({
     duration: 800,
